e2e26366cce47dbf306d36345025b34327edb2ac
stevengodev <stevengodev@gmail.com>
2024-12-21 00:13:38 -0500

    Corrige los repositorios AutorRepository y LibroRepository

--- src/main/java/com/foliaco/proyecciones/repository/AutorRepository.java
+++ src/main/java/com/foliaco/proyecciones/repository/AutorRepository.java
@@ -8,27 +8,28 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.foliaco.proyecciones.entity.AutorEntity;
-import com.foliaco.proyecciones.interfaces.AutorBiografiaCompleta;
 import com.foliaco.proyecciones.interfaces.AutorResumen;
+
+import jakarta.persistence.Tuple;
 
 @Repository
 public interface AutorRepository extends JpaRepository<AutorEntity, Long> {
     
     @Query(value = "SELECT a.nombre AS nombre, a.apellido AS apellido, COUNT(l) AS cantidadLibros " +
-           "FROM Autor a LEFT JOIN a.libros l " +
+           "FROM AutorEntity a LEFT JOIN a.libros l " +
            "GROUP BY a.nombre")
     List<AutorResumen> listarResumenDeAutores();
 
     @Query(value = "SELECT a.nombre AS nombre, a.apellido AS apellido, COUNT(l) AS cantidadLibros " +
-           "FROM Autor a LEFT JOIN a.libros l " +
+           "FROM AutorEntity a LEFT JOIN a.libros l " +
            "WHERE a.id = :id " +
            "GROUP BY a.nombre")
     AutorResumen buscarResumenDeAutorPorId(@Param("id") Long id);
 
     @Query(value = "SELECT a.nombre AS nombre, a.apellido AS apellido, a.nacionalidad AS nacionalidad, a.sexo AS sexo, COUNT(l) AS cantidadLibros " +
-           "FROM Autor a LEFT JOIN a.libros l " +
+           "FROM AutorEntity a LEFT JOIN a.libros l " +
            "WHERE a.id = :id " +
            "GROUP BY a.nombre")
-    AutorBiografiaCompleta buscarBiografiaDeAutorPorId(@Param("id") Long id);
+    Tuple buscarBiografiaDeAutorPorId(@Param("id") Long id);
 
 }
--- src/main/java/com/foliaco/proyecciones/repository/LibroRepository.java
+++ src/main/java/com/foliaco/proyecciones/repository/LibroRepository.java
@@ -8,12 +8,14 @@ import com.foliaco.proyecciones.entity.LibroEntity;
 import com.foliaco.proyecciones.interfaces.LibroResumen;
 
 import jakarta.persistence.Tuple;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LibroRepository extends JpaRepository<LibroEntity, Long> {
     
 
     @Query(value = "SELECT l.titulo AS titulo, l.anioPublicacion AS anioPublicacion, l.genero AS genero, a.nombre AS autor " +
-           "FROM Libro l LEFT JOIN l.autor a " +
+           "FROM LibroEntity l LEFT JOIN l.autor a " +
            "WHERE l.id = :id")
     LibroResumen buscarResumenDeLibroPorId(@Param("id") Long id);
 
@@ -22,12 +24,12 @@ public interface LibroRepository extends JpaRepository<LibroEntity, Long> {
     En lugar de obtener los campos podriamos devolver directamente el dto de la siguiente manera, sin embargo,
     en este caso se opto por devolver un Tuple para obtener los campos de una forma mas facil
     @Query(value = "SELECT new com.foliaco.proyecciones.dto.LibroResumenDto(l.titulo, l.anioPublicacion, l.genero, a.nombre) " +
-    "FROM Libro l LEFT JOIN l.autor a " +
+    "FROM LibroEntity l LEFT JOIN l.autor a " +
     "WHERE l.id = :id")
     */
 
     @Query(value = "SELECT l.titulo AS titulo, l.anioPublicacion AS anioPublicacion, l.genero AS genero, a.nombre AS autor " +
-           "FROM Libro l LEFT JOIN l.autor a " +
+           "FROM LibroEntity l LEFT JOIN l.autor a " +
            "WHERE l.id = :id")
     Tuple buscarLibroDtoPorId(@Param("id") Long id);
 
