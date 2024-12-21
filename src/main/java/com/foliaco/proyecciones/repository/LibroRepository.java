ab9e2942f89f441accec200d22231e4dd671b18f
stevengodev <stevengodev@gmail.com>
2024-12-20 23:26:50 -0500

    Actualiza el repositorio LibroRepository

--- src/main/java/com/foliaco/proyecciones/repository/LibroRepository.java
+++ src/main/java/com/foliaco/proyecciones/repository/LibroRepository.java
@@ -2,9 +2,12 @@ package com.foliaco.proyecciones.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.foliaco.proyecciones.entity.LibroEntity;
 import com.foliaco.proyecciones.interfaces.LibroResumen;
+
+import jakarta.persistence.Tuple;
 
 public interface LibroRepository extends JpaRepository<LibroEntity, Long> {
     
@@ -12,7 +15,20 @@ public interface LibroRepository extends JpaRepository<LibroEntity, Long> {
     @Query(value = "SELECT l.titulo AS titulo, l.anioPublicacion AS anioPublicacion, l.genero AS genero, a.nombre AS autor " +
            "FROM Libro l LEFT JOIN l.autor a " +
            "WHERE l.id = :id")
-    LibroResumen buscarResumenDeLibroPorId(Long id);
+    LibroResumen buscarResumenDeLibroPorId(@Param("id") Long id);
 
+    /**
+    proyeccion basada en clases, especificamente en dto
+    En lugar de obtener los campos podriamos devolver directamente el dto de la siguiente manera, sin embargo,
+    en este caso se opto por devolver un Tuple para obtener los campos de una forma mas facil
+    @Query(value = "SELECT new com.foliaco.proyecciones.dto.LibroResumenDto(l.titulo, l.anioPublicacion, l.genero, a.nombre) " +
+    "FROM Libro l LEFT JOIN l.autor a " +
+    "WHERE l.id = :id")
+    */
+
+    @Query(value = "SELECT l.titulo AS titulo, l.anioPublicacion AS anioPublicacion, l.genero AS genero, a.nombre AS autor " +
+           "FROM Libro l LEFT JOIN l.autor a " +
+           "WHERE l.id = :id")
+    Tuple buscarLibroDtoPorId(@Param("id") Long id);
 
 }
