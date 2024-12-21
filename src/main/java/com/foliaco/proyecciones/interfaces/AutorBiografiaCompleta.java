4bdc1c3583c4ce52f2f3064b25ad6c432ec0930b
stevengodev <stevengodev@gmail.com>
2024-12-20 23:05:26 -0500

    Agrega la interfaz de proyeccion abierta AutorBiografiaCompleta

---
+++ src/main/java/com/foliaco/proyecciones/interfaces/AutorBiografiaCompleta.java
@@ -0,0 +1,11 @@
+package com.foliaco.proyecciones.interfaces;
+
+import org.springframework.beans.factory.annotation.Value;
+
+// proyeccion abierta
+public interface AutorBiografiaCompleta {
+    
+    @Value("#{target.nombre} + ' ' + #{target.apellido} + ' ' + #{target.nacionalidad} + ' ' + #{target.sexo} + ' ' + #{target.cantidadLibros}")
+    String getBiografia();
+    
+}
